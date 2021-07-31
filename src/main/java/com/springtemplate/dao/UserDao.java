0237168c3727e650ad6fcedca723a894446553b1
Siew Joe Kane <joekanesiew@outlook.com>
2021-07-31 09:24:41 +0800

    remove unused import

--- src/main/java/com/springtemplate/dao/UserDao.java
+++ src/main/java/com/springtemplate/dao/UserDao.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
