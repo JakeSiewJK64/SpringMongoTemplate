17d1edf274a79702d6d8dc6a2b00952e3362540f
Siew Joe Kane <joekanesiew@outlook.com>
2021-07-31 16:48:03 +0800

    update login screen

--- ClientApp/FrontEnd/src/app/app.module.ts
+++ ClientApp/FrontEnd/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -12,14 +13,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   declarations: [
     AppComponent,
     LoginComponent,
-    MaterialModule,
-    FlexLayoutModule,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
     FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
