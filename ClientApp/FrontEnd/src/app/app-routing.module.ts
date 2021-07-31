fc620af7b9b8be457906c6fad4a305f42211071d
Siew Joe Kane <joekanesiew@outlook.com>
2021-07-31 16:31:28 +0800

    added required npm packages

--- ClientApp/FrontEnd/src/app/app-routing.module.ts
+++ ClientApp/FrontEnd/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "authentication/login",
+    component: LoginComponent,
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientApp/FrontEnd/src/app/app.module.ts
+++ ClientApp/FrontEnd/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MaterialModule,
+    FlexLayoutModule,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
