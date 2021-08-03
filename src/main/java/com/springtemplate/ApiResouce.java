a48c018842867356b499695074f4893ca9056ffa
Siew Joe Kane <joekanesiew@outlook.com>
2021-08-03 17:43:59 +0800

    renamed HomeResource to ApiResource

--- src/main/java/com/springtemplate/ApiResouce.java
+++ src/main/java/com/springtemplate/ApiResouce.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeResource {
+public class ApiResouce {
 
     @Autowired
     private AuthenticationManager authenticationManager;
